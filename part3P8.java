1eda3b9ab79769f99f4b81a64790425b2eac0c6d
XIG <xig84@pitt.edu>
2024-02-18 17:23:28 -0500

    part3P8

---
+++ part3P8.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class part3P8 {
+
+    public static class DateMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        private final static IntWritable bytes = new IntWritable();
+        private Text date = new Text();
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            if (line.contains("19/Dec/2020")) {
+                String[] parts = line.split("\"");
+                if (parts.length >= 3) {
+                    String[] sizeParts = parts[2].split("\\s+");
+                    if (sizeParts.length >= 2) {
+                        try {
+                            bytes.set(Integer.parseInt(sizeParts[1]));
+                            date.set("19/Dec/2020");
+                            context.write(date, bytes);
+                        } catch (NumberFormatException e) {
+                            // Ignore lines where size is not a number
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static class DataReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Data Requested On 19/Dec/2020");
+        job.setJarByClass(part3P8.class);
+        job.setMapperClass(DateMapper.class);
+        job.setCombinerClass(DataReducer.class);
+        job.setReducerClass(DataReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
